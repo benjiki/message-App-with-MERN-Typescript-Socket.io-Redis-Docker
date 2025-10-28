73e957407cd600637f8f5fa04ebfd0ce475aa3b5
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-10-28 18:09:58 +0300

    updated the guard

--- frontend/src/PageGuards.tsx
+++ frontend/src/PageGuards.tsx
@@ -1,14 +1,21 @@
 import { Navigate, Outlet } from "react-router";
-import { useAuthStore } from "./stores/authStore";
+
+import { useAuth } from "./hooks/useAuth";
 
 // Only accessible if the user is authenticated
 export function PrivateRoute() {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  return isAuthenticated ? <Outlet /> : <Navigate to="/auth" replace />;
+  const { data: user, isLoading, isError } = useAuth();
+  if (isLoading) return <div>Loding...</div>;
+
+  if (isError || !user) return <Navigate to="/auth" />;
+
+  return <Outlet />;
 }
 
 // Only accessible if the user is NOT authenticated
 export function GuestRoute() {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  return !isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
+  const { data: user, isLoading } = useAuth();
+  if (isLoading) return <div>Loding...</div>;
+
+  return !user ? <Outlet /> : <Navigate to="/auth" />;
 }
